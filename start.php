52fdc37a1b950fa7c4c0f245bb2049d62add2c61
hcastror.srv <hcastror.srv@ips.gob.cl>
2021-04-23 00:49:22 -0400

    Chabge URL

--- start.php
+++ start.php
@@ -1,7 +1,7 @@
 <?php
 //aqui conexion sql server
 require_once('conexion.php');
-require_once('/opt/app-root/src/libs/adlap/adldap.php');
+require_once('/libs/adlap/adldap.php');
 require_once('libs/adlap/adldap_functions.php');
 
 ini_set('error_reporting', E_ALL & ~(E_NOTICE | E_DEPRECATED | E_STRICT));
